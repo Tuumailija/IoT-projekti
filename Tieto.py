72cecb454b74c834846560ca7405a2ed7b0ded31
Markus <149786984+Tuumailija@users.noreply.github.com>
2024-04-18 11:57:04 +0300

    Add files via upload

---
+++ Tieto.py
@@ -0,0 +1,30 @@
+from flask import Flask, jsonify
+import requests
+import json
+
+app = Flask(__name__)
+
+API_KEY = 'Z9HL1JG0Y6TPT1BI'
+CHANNEL_ID = '2504141'
+
+@app.route('/')
+def get_data_from_thingspeak():
+    try:
+        response = requests.get(f'https://api.thingspeak.com/channels/{CHANNEL_ID}/feeds.json?api_key={API_KEY}&results=1')
+        data = response.json()
+
+        latest_entry = data['feeds'][0]
+
+        formatted_data = {
+            'Sisalampotila': latest_entry.get('field1'),
+            'Ulkolampotila': latest_entry.get('field2'),
+            'Ilmankosteus sisalla': latest_entry.get('field3'),
+            'Ilmankosteus ulkona': latest_entry.get('field4'),
+        }
+
+        return jsonify(formatted_data), 20
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+if __name__ == '__main__':
+    app.run(debug=True)
